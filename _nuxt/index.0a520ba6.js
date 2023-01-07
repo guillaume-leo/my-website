5125be84723c2f499058661265356ac9407d8a4a
guillaume <quet.guillaume.leo@gmail.com>
2023-01-07 09:55:02 +0100

    77b7a06

---
+++ _nuxt/index.0a520ba6.js
@@ -0,0 +1,1 @@
+import{a as e}from"./entry.50a484da.js";const n={};function r(c,t){return null}const o=e(n,[["render",r]]);export{o as default};
---
+++ _nuxt/index.eee5ead6.js
@@ -0,0 +1,1 @@
+import{G as e,b as n,o}from"./entry.50a484da.js";const s={__name:"index",setup(t){return e(async()=>{}),(a,c)=>(o(),n("h1",null,"Home"))}};export{s as default};
